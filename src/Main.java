dc5c22fbf0cd32251ee23d199d471343847ea880
Serdyukov <and.serdykov@gmail.com>
2022-09-01 18:42:07 +0300

    Задание №1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,9 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String firstName = "Ivan";
+        String middleName = "Ivanovich";
+        String lastName = "Ivanov";
+        String fullName = lastName + " " + firstName + " " + middleName;
+        System.out.println(fullName);
     }
 }
